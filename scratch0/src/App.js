b9352824b22331fb629a58cdac76eaf28e7aa8e2
Chanthy Ngin <nginchanthy@gmail.com>
2018-07-17 19:41:43 -0700

    add file-loader

--- scratch0/src/App.js
+++ scratch0/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import pic1 from './static/images/space-needle.jpg';
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello</h1>;
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <img src={pic1} alt="" width="300" />
+    </div>
+  );
+};
 
 export default App;
